166f9dda8439f624dfeb3823a886742a499df6e7
Eendex <sherowin.clemons@gmail.com>
2024-12-11 16:52:25 -0500

    Finishing up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ApiPlayer.Models
+{
+    public class ErrorViewModel
+    { 
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
